f2f2dfefa1157cdcc6068f6ff18e08a4ec88e5a7
Christomanos Anastasiou <hristoma@gmail.com>
2025-10-04 19:31:33 +0100

    Bright up active buttons, hide inactive bomb buttons, Add grid variations based for the different difficulties. Add Place holder images for key and bomb

--- MinesweeperCasinoGame/Assets/Script/ButtonCell.cs
+++ MinesweeperCasinoGame/Assets/Script/ButtonCell.cs
@@ -1,30 +1,44 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class ButtonCell : MonoBehaviour
 {
+    [SerializeField] private GameObject grenadeImage;
+    [SerializeField] private GameObject keyImageContainer;
+    [SerializeField] private Image keyImage;
+    [SerializeField] private Image keyBackgroundImage;
+
     private ButtonType buttonType;
     private Button button;
-    private Image image;
+    private Image buttonImage;
 
-    private Color KeyColor = Color.green;
-    private Color BombColor = Color.red;
+    private Color keyColor = Color.green;
+    private Color bombColor = Color.red;
+
     public UnityEvent OnClick => button.onClick;
+
+    public Action playerLoses;
+    public Action playerFoundKey;
+
+    private bool isActive;
 
     private void Awake()
     {
         button = GetComponent<Button>();
-        image = GetComponent<Image>();
+        buttonImage = GetComponent<Image>();
 
         OnClick.AddListener(OnClicked);
     }
 
-
-    public void Initialize(ButtonType buttonType)
+    public void Initialize(ButtonType buttonType, Action onPlayerLoses, Action onPlayerFoundKey)
     {
-
+        playerFoundKey = onPlayerFoundKey;
+        playerLoses = onPlayerLoses;
         this.buttonType = buttonType;
+
+        ResetCell();
     }
 
     public void SetInteractable(bool interactable)
@@ -34,14 +48,51 @@ public class ButtonCell : MonoBehaviour
 
     private void OnClicked()
     {
+        if (buttonType == ButtonType.BOMB)
+            playerLoses?.Invoke();
+        else
+            playerFoundKey?.Invoke();
+
+        RevealType();
+    }
+
+    public void RevealType()
+    {
+        isActive = true;
+
         switch(buttonType)
         {
             case ButtonType.KEY:
-                image.color = KeyColor;
+                buttonImage.color = keyColor;
+                keyImageContainer.SetActive(true);
                 break;
             case ButtonType.BOMB:
-                image.color = BombColor;
+                buttonImage.color = bombColor;
+                grenadeImage.SetActive(true);
                 break;
         }
+    }
+
+    public void RevealHiddenType()
+    {
+        if(isActive)
+            return;
+
+        switch(buttonType)
+        {
+            case ButtonType.KEY:
+                buttonImage.color = new Color(keyColor.r, keyColor.g, keyColor.b, 0.5f);
+                keyImage.color = new Color(keyImage.color.r, keyImage.color.g, keyImage.color.b, 0.5f);
+                keyBackgroundImage.color = new Color(keyBackgroundImage.color.r, keyBackgroundImage.color.g, keyBackgroundImage.color.b, 0.5f);
+                keyImageContainer.SetActive(true);
+                break;
+        }
+    }
+
+    public void ResetCell()
+    {
+        buttonImage.color = Color.white;
+        keyImageContainer.SetActive(false);
+        grenadeImage.SetActive(false);
     }
 }
--- MinesweeperCasinoGame/Assets/Script/GridManager.cs
+++ MinesweeperCasinoGame/Assets/Script/GridManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GridManager : MonoBehaviour
 {
@@ -12,51 +11,90 @@ public class GridManager : MonoBehaviour
 
     private ButtonCell[,] grid;
 
-    int currentRow = 0;
+    private int currentRow = 0;
 
-    void Start()
+    private void Awake()
     {
         grid = new ButtonCell[width, height];
+    }
 
+    private void Start()
+    {
         GenerateGrid();
     }
 
-    void GenerateGrid()
+    private void GenerateGrid()
     {
         for(int h = 0; h < height; h++)
         {
-            var badCell = GetBadCell();
+            var bombCellIndex = GetBombCellIndex();
             for(int w = 0; w < width; w++)
             {
                 ButtonCell buttonCell = Instantiate(button, new Vector3(w * space, h * space, 0), Quaternion.identity, gridPlaceholder.transform).GetComponent<ButtonCell>();
+
+                buttonCell.gameObject.SetActive(false);
+
+                buttonCell.SetInteractable(h == currentRow);
+
+                buttonCell.gameObject.SetActive(true);
 
                 grid[w, h] = buttonCell;
 
                 buttonCell.name = $"Cell {w},{h}";
 
-                buttonCell.Initialize(w == badCell ? ButtonType.BOMB : ButtonType.KEY);
-
-                buttonCell.SetInteractable(w < width && h == currentRow);
-
-                buttonCell.OnClick.AddListener(OnButtonClicked);
+                buttonCell.Initialize(w == bombCellIndex ? ButtonType.BOMB : ButtonType.KEY, OnGameEnds, OnPlayerFoundKey);
             }
         }
     }
 
-    void OnButtonClicked()
+    private void OnPlayerFoundKey()
     {
         currentRow++;
+
+        if(currentRow >= height)
+        {
+            Debug.Log("Player Wins!");
+            OnGameEnds();
+            return;
+        }
+
         for(int h = 0; h < height; h++)
         {
             for(int w = 0; w < width; w++)
             {
-                grid[w, h].SetInteractable(w < width && h == currentRow);
+                grid[w, h].SetInteractable(h == currentRow);
             }
         }
     }
 
-    float GetBadCell()
+    private void OnGameEnds()
+    {
+        for(int h = 0; h < height; h++)
+        {
+            for(int w = 0; w < width; w++)
+            {
+                grid[w, h].RevealHiddenType();
+                grid[w, h].SetInteractable(false);
+            }
+        }
+    }
+
+    private int GetBombCellIndex()
     {
         return Random.Range(0, width);
+    }
+
+    public void ResetGrid()
+    {
+        currentRow = 0;
+        for(int h = 0; h < height; h++)
+        {
+            var bombCellIndex = GetBombCellIndex();
+            for(int w = 0; w < width; w++)
+            {
+                grid[w, h].Initialize(w == bombCellIndex ? ButtonType.BOMB : ButtonType.KEY, OnGameEnds, OnPlayerFoundKey);
+                grid[w, h].SetInteractable(h == currentRow);
+            }
+        }
     }
 }
